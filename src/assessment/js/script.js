ab6aecfdb2252f1e0b87f9223b0d41aef784c934
nagasawaaaa <snagasaw.sp.create@gmail.com>
2018-02-12 23:09:13 +0900

    [N予備校] 診断機能の組込み

--- src/assessment/js/script.js
+++ src/assessment/js/script.js
@@ -1,5 +1,38 @@
 (function() {
   'use strict';
+  const userNameInput = document.getElementById('user-name');
+  const assessmentButton = document.getElementById('assessment');
+  const resultDivided = document.getElementById('result-area');
+  const tweetDivided = document.getElementById('tweet-area');
+
+  /**
+   * 指定した要素の子どもを全て削除する
+   * @param {HTMLElement} element HTMLの要素
+   */
+  function removeAllChildren(element) {
+    while (element.firstChild) { // 子どもの要素があるかぎり削除
+      element.removeChild(element.firstChild);
+    }
+  }
+
+  assessmentButton.onclick = () => {
+    const userName = userNameInput.value;
+    if(userName.length === 0) { // 名前が空の時は処理を終了する
+      return;
+    }
+
+    // TODO 診断結果表示エリアの作成
+    removeAllChildren(resultDivided);
+    removeAllChildren(tweetDivided);
+    const header = document.createElement('h3');
+    header.innerText = '診断結果';
+    resultDivided.appendChild(header);
+
+    const paragraph = document.createElement('p');
+    const  result = assessment(userName);
+    paragraph.innerText = result;
+    resultDivided.appendChild(paragraph);
+  }
 
   const answers = [
     '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
